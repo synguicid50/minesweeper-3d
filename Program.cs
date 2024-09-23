a52ec24853164d0d26b8f0b1eb5a76f2197449a2
synguicid50 <synguicid50@gmail.com>
2024-09-23 15:03:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿Console.CursorVisible = false;
+Console.Title = "Minesweeper 3D";
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+//TODO: configure a display buffer
+List<int>? ConfigureLatticeDimensions()
+{
+    //beginner 9^3 intermediate 16^3 expert 25^3
+    List<string> defaultDiffLevels = new List<string>
+    {
+        "Beginner      ",
+        "Intermediate  ",
+        "Expert        "
+    };
+
+    bool inputReceived = false;
+    int arrowPos = 0;
+
+    while (!inputReceived)
+    {
+        Console.Clear();
+        Console.Write("Choose difficulty:");
+
+        for (int i = 0; i < defaultDiffLevels.Count; i++)
+        {
+            Console.Write($"\n{defaultDiffLevels[i]}");
+            if (arrowPos == i)
+            {
+                Console.Write("\u2190");
+            }
+        }
+
+        ConsoleKey userInput = Console.ReadKey().Key;
+
+        switch (userInput)
+        {
+            case ConsoleKey.W:
+            case ConsoleKey.UpArrow:
+                if (arrowPos > 0)
+                {
+                    arrowPos--;
+                }
+                break;
+            case ConsoleKey.S:
+            case ConsoleKey.DownArrow:
+                if (arrowPos < defaultDiffLevels.Count - 1)
+                {
+                    arrowPos++;
+                }
+                break;
+            case ConsoleKey.Spacebar:
+            case ConsoleKey.Enter:
+                inputReceived = true;
+                break;
+        }
+    }
+
+    switch (arrowPos)
+    {
+        case 0:
+            return new List<int> { 9, 9, 9 };
+        case 1:
+            return new List<int> { 16, 16, 16 };
+        case 2:
+            return new List<int> { 25, 25, 25 };
+        default:
+            return null; //type? is nullable, not that this is ever going to return null
+    }
+}
