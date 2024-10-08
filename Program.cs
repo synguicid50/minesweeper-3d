7de75c7b198f5a15bfe931656eaeac0299840366
synguicid50 <synguicid50@gmail.com>
2024-10-08 22:29:34 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ ConfigureLattice(ref latticeDimensions, ref mineCount);
 Console.Clear();
 
 Dictionary<(int x, int y, int z), (int tileData, bool isVisible, bool isFlagged)> latticeMap = new();
+ConfigureLatticeData(ref latticeMap);
 
 
 void ConfigureLattice(ref (int, int, int) latticeDimensions, ref int mineCount)
@@ -84,7 +85,7 @@ void ConfigureLattice(ref (int, int, int) latticeDimensions, ref int mineCount)
     }
 }
 
-void ConfigureTileData(ref Dictionary<(int x, int y, int z), (int tileData, bool isVisible, bool isFlagged)> latticeMap)
+void ConfigureLatticeData(ref Dictionary<(int x, int y, int z), (int tileData, bool isVisible, bool isFlagged)> latticeMap)
 {
     for (int i = 0; i < latticeDimensions.x; i++)
     {
@@ -96,4 +97,56 @@ void ConfigureTileData(ref Dictionary<(int x, int y, int z), (int tileData, bool
             }
         }
     }
+
+    Random mineRng = new Random();
+
+    int placedMines = 0;
+
+    List<(int, int, int)> mineTiles = new();
+
+    while (placedMines < mineCount)
+    {
+        var randomTile = (mineRng.Next(latticeDimensions.x), mineRng.Next(latticeDimensions.y), mineRng.Next(latticeDimensions.z));
+
+        if (latticeMap[randomTile].tileData != 9)
+        {
+            latticeMap[randomTile] = (9, latticeMap[randomTile].isVisible, latticeMap[randomTile].isFlagged);
+            mineTiles.Add(randomTile);
+            placedMines++;
+        }
+    }
+
+    foreach (var mineTile in mineTiles)
+    {
+        var adjacentTiles = FindAdjacentTiles(mineTile);
+        foreach (var adjacentTile in adjacentTiles)
+        {
+            if (latticeMap[adjacentTile].tileData != 9)
+            {
+                latticeMap[adjacentTile] = (latticeMap[adjacentTile].tileData + 1, latticeMap[adjacentTile].isVisible, latticeMap[adjacentTile].isFlagged);
+            }
+        }
+    }
+
+}
+
+List<(int, int, int)> FindAdjacentTiles((int x, int y, int z) tile)
+{
+    List<(int, int, int)> adjacentTiles = new();
+
+    for (int i = -1; i < 1; i += 2)
+    {
+        for (int j = -1; j < 1; j += 2)
+        {
+            for (int k = -1; k < 1; k += 2)
+            {
+                if (latticeMap.ContainsKey((tile.x + i, tile.y + j, tile.z + k)))
+                {
+                    adjacentTiles.Add((tile.x + i, tile.y + j, tile.z + k));
+                }
+            }
+        }
+    }
+
+    return adjacentTiles;
 }
