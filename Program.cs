dc073278c6c7a460f463b19774613fe0a0270217
synguicid50 <synguicid50@gmail.com>
2024-10-09 22:04:56 +0300

    ternary operaters go brrr

--- Program.cs
+++ Program.cs
@@ -4,12 +4,17 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 var latticeDimensions = (x: 0, y: 0, z: 0);
 int mineCount = 0;
+int flagCount = 0;
 
 ConfigureLattice(ref latticeDimensions, ref mineCount);
 Console.Clear();
 
 Dictionary<(int x, int y, int z), (int tileData, bool isVisible, bool isFlagged)> latticeMap = new();
 ConfigureLatticeData(ref latticeMap);
+
+int view = 1;
+var focusTile = (x:2, y:2, z:2); //todo place this at the approximate center
+DisplayHUD(1);
 
 void ConfigureLattice(ref (int, int, int) latticeDimensions, ref int mineCount)
 {
@@ -83,7 +88,6 @@ void ConfigureLattice(ref (int, int, int) latticeDimensions, ref int mineCount)
             break;
     }
 }
-
 void ConfigureLatticeData(ref Dictionary<(int x, int y, int z), (int tileData, bool isVisible, bool isFlagged)> latticeMap)
 {
     for (int i = 0; i < latticeDimensions.x; i++)
@@ -128,7 +132,6 @@ void ConfigureLatticeData(ref Dictionary<(int x, int y, int z), (int tileData, b
     }
 
 }
-
 List<(int, int, int)> FindAdjacentTiles((int x, int y, int z) tile)
 {
     List<(int, int, int)> adjacentTiles = new();
@@ -150,4 +153,71 @@ List<(int, int, int)> FindAdjacentTiles((int x, int y, int z) tile)
     adjacentTiles.Remove(tile);
 
     return adjacentTiles;
+}
+
+void Sweep()
+{
+    bool gameOver = false;
+    bool gameWon = false;
+
+    while (!gameOver)
+    {
+        ConsoleKey userInput = Console.ReadKey().Key;
+
+        switch (userInput)
+        {
+            case ConsoleKey.D1:
+                view = 1;
+                break;
+            case ConsoleKey.D2:
+                view = 2;
+                break;
+            case ConsoleKey.D3:
+                view = 3;
+                break;
+            case ConsoleKey.Q:
+                
+                break;
+            case ConsoleKey.E:
+
+                break;
+        }
+    }
+}
+
+void DisplayLattice()
+{
+
+}
+
+void DisplayHUD(int view)
+{
+    Console.WriteLine($" Move around in the grid with [WASD] or [\u2190\u2191\u2192\u2193], move between layers with [Q] and [E].\n Press [SPACE] to destroy a tile, press [F] to place or remove a flag.");
+
+    Console.Write(" View: ");
+
+    for (int i = 1; i <= 3; i++)
+    {
+        if (i == view)
+        {
+            Console.Write($"{{{i}}}");
+        }
+        else
+        {
+            Console.Write($" {i} ");
+        }
+    }
+
+    Console.Write($"    [*] {mineCount - flagCount} ");
+
+    if (mineCount - flagCount < 100)
+    {
+        Console.Write(" ");
+        if (mineCount - flagCount < 10)
+        {
+            Console.Write(" ");
+        }
+    }
+
+    Console.Write($"[\u2690] {flagCount}");
 }
