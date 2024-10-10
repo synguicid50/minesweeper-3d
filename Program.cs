f3bb47fead7126b4550b1b33af7fb1947cf9013b
synguicid50 <synguicid50@gmail.com>
2024-10-10 10:21:22 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 var latticeDimensions = (x: 0, y: 0, z: 0);
 int mineCount = 0;
 int flagCount = 0;
+int visibleTileCount = 0;
 
 ConfigureLattice(ref latticeDimensions, ref mineCount);
 Console.Clear();
@@ -14,6 +15,7 @@ ConfigureLatticeData(ref latticeMap);
 
 int view = 1;
 var focusTile = (x: (latticeDimensions.x - (latticeDimensions.x % 2)) / 2, y: (latticeDimensions.y - (latticeDimensions.y % 2)) / 2, z: (latticeDimensions.z - (latticeDimensions.z % 2)) / 2);
+
 Sweep();
 
 void ConfigureLattice(ref (int, int, int) latticeDimensions, ref int mineCount)
@@ -155,6 +157,57 @@ List<(int, int, int)> FindAdjacentTiles((int x, int y, int z) tile)
     return adjacentTiles;
 }
 
+void ChainClear((int x, int y, int z) tile)
+{
+    if (latticeMap[tile].isVisible) return;
+
+    foreach (var adjacentTile in FindAdjacentTiles(tile))
+    {
+        if (!latticeMap[adjacentTile].isVisible && latticeMap[adjacentTile].tileData == 0)
+        {
+            ChainClear(adjacentTile);
+        }
+    }
+}
+
+
+
+void DisplayHUD()
+{
+    Console.WriteLine($" Move around in the layer with [WASD] or [\u2190\u2191\u2192\u2193], move between layers with [Q] and [E].\n Press [SPACE] to destroy a tile, press [F] to place or remove a flag.");
+
+    Console.Write(" View: ");
+
+    for (int i = 1; i <= 3; i++)
+    {
+        if (i == view)
+        {
+            Console.Write($"{{{i}}}");
+        }
+        else
+        {
+            Console.Write($" {i} ");
+        }
+    }
+
+    Console.Write($"    [*] {mineCount - flagCount}    ");
+
+    if ((mineCount - flagCount) < 100 && (mineCount - flagCount) > -100)
+    {
+        Console.Write(" ");
+        if ((mineCount - flagCount) < 10 && (mineCount - flagCount) > -10)
+        {
+            Console.Write(" ");
+        }
+    }
+
+    Console.Write($"[\u2690] {flagCount}\n");
+}
+void DisplayLattice()
+{
+
+}
+
 void Sweep()
 {
     bool gameOver = false;
@@ -203,51 +256,19 @@ void Sweep()
                 focusTile = (view == 3) ? ((focusTile.y < latticeDimensions.y - 1) ? (focusTile.x, focusTile.y + 1, focusTile.z) : focusTile) : ((focusTile.x > 0) ? (focusTile.x - 1, focusTile.y, focusTile.z) : (focusTile));
                 break;
             case ConsoleKey.Spacebar:
-                if (!latticeMap[focusTile].isVisible)
+                if (!latticeMap[focusTile].isVisible && !latticeMap[focusTile].isFlagged)
                 {
                     latticeMap[focusTile] = (latticeMap[focusTile].tileData, true, latticeMap[focusTile].isFlagged);
+                    if (latticeMap[focusTile].tileData == 0)
+                    {
+                        ChainClear(focusTile);
+                    }
                 }
                 break;
             case ConsoleKey.F:
                 latticeMap[focusTile] = (latticeMap[focusTile].tileData, latticeMap[focusTile].isVisible, !latticeMap[focusTile].isFlagged);
                 break;
         }
-     
+
     }
-}
-
-void DisplayHUD()
-{
-    Console.WriteLine($" Move around in the layer with [WASD] or [\u2190\u2191\u2192\u2193], move between layers with [Q] and [E].\n Press [SPACE] to destroy a tile, press [F] to place or remove a flag.");
-
-    Console.Write(" View: ");
-
-    for (int i = 1; i <= 3; i++)
-    {
-        if (i == view)
-        {
-            Console.Write($"{{{i}}}");
-        }
-        else
-        {
-            Console.Write($" {i} ");
-        }
-    }
-
-    Console.Write($"    [*] {mineCount - flagCount}    ");
-
-    if (mineCount - flagCount < 100)
-    {
-        Console.Write(" ");
-        if (mineCount - flagCount < 10)
-        {
-            Console.Write(" ");
-        }
-    }
-
-    Console.Write($"[\u2690] {flagCount}\n");
-}
-void DisplayLattice()
-{
-
 }
